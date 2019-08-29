d01faef7ae3c360ed8ebce845fa9b7ffd505bcee
iLLiCiTiT <jakub.trllo@gmail.com>
2019-08-29 18:24:01 +0200

    addded proxy family filter to library loader models as it uses family config

--- avalon/tools/libraryloader/models/__init__.py
+++ avalon/tools/libraryloader/models/__init__.py
@@ -4,10 +4,14 @@ from .model_task import TaskModel
 from .model_asset import AssetModel
 from .model_subset import SubsetModel
 
+from .proxy_family_filter import FamilyFilterProxyModel
+
 __all__ = [
     "lib",
 
     "TaskModel",
     "AssetModel",
-    "SubsetModel"
+    "SubsetModel",
+
+    "FamilyFilterProxyModel"
 ]
