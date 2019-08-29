57a04f4237ce289803c5c228277d1f5adee87caf
iLLiCiTiT <jakub.trllo@gmail.com>
2019-08-29 16:17:11 +0200

    added lib to models with grouping methods

--- avalon/tools/libraryloader/models/__init__.py
+++ avalon/tools/libraryloader/models/__init__.py
@@ -1,9 +1,11 @@
 from .model_tasks import TasksModel
+from . import lib
 from .model_asset import AssetModel
 from .model_subsets import SubsetsModel
 
 __all__ = [
     "TasksModel",
+    "lib",
     "AssetModel",
     "SubsetsModel",
 ]
